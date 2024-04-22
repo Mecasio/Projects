34a03f0f5bdc1690445767d4198b5992fc9d55fa
Mecasio <mecasio.a.bsinfotech@gmail.com>
2024-04-22 11:13:20 +0800

    Projects

--- dcrl/views.py
+++ dcrl/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import SignUpForm
 
 def home(request):
 
     return render(request, 'home.html')
+
+def register(request):
+
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+        
+    return render(request, 'register.html')
