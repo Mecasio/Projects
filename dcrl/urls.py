2f897964df799618965615595ed2f7c3ec1a673e
Mecasio <mecasio.a.bsinfotech@gmail.com>
2024-05-06 15:24:17 +0800

    Projects

--- dcrl/urls.py
+++ dcrl/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('register', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('logout/', views.logout_user, name='logout'),
+    path('add_record/', views.add_record, name='add_record'),
+    path('update_record/<int:pk>', views.update_record, name='update_record'),
+    path('delete_record/<int:pk>', views.delete_record, name='delete_record'),
 ]
